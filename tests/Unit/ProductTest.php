bbd79081c4ee5ed2a81c5df3df0f50c5cd423cb5
giuseppe <peppex.2502@gmail.com>
2022-09-22 21:19:29 +0200

    added web middleware to fix authentication issues

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -3,9 +3,12 @@
 namespace Giuszeppe\AwesomeLaravelCart\Tests\Unit;
 
 use Giuszeppe\AwesomeLaravelCart\Facades\Item;
+use Giuszeppe\AwesomeLaravelCart\Models\Cart;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Giuszeppe\AwesomeLaravelCart\Tests\TestCase;
 use Giuszeppe\AwesomeLaravelCart\Tests\TestModels\Product;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ProductTest extends TestCase
 {
@@ -18,5 +21,26 @@ class ProductTest extends TestCase
         $product->name = "Ez";
         $product->save();
         $this->assertEquals($product->name, 'Ez');
+    }
+    /** @test */
+    function a_product_has_carts()
+    {
+        $product = Item::factory()
+            ->has(Cart::factory())
+            ->create();
+        $product->name = "Ez";
+        $product->save();
+        $this->assertInstanceOf(BelongsToMany::class, $product->carts());
+        $this->assertNotNull($product->carts[0]);
+    }
+    /** @test */
+    function a_product_has_a_cart_pivot_table()
+    {
+        $product = Item::factory()
+            ->has(Cart::factory())
+            ->create();
+        $product->name = "Ez";
+        $product->save();
+        $this->assertNotNull($product->carts[0]->pivot);
     }
 }
